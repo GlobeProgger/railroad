f81e7c7261128690351d3ac35ca288008b5a5900
globeprogger <globeprogger@gmail.com>
2015-05-20 01:35:55 +0200

    added * 'Back' links to articles.new and articles..show * form data validation and error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,26 @@
 class ArticlesController < ApplicationController
 
-	  def index
-	    @articles = Article.all
-	  end
+  def index
+    @articles = Article.all
+  end
 
-	  def show
-	    @article = Article.find(params[:id])
-	  end
+  def show
+    @article = Article.find(params[:id])
+  end
 
 	def new
+    @article = Article.new
 	end
 
 	def create
 	  @article = Article.new(article_params)
 	 
-	  @article.save
-	  redirect_to @article
-	end
+	  if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+end
  
 	private
 	  def article_params
