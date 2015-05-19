de5c61fae9bc22209c97a851492b41e0f0c8e0a8
globeprogger <globeprogger@gmail.com>
2015-05-19 03:28:30 +0200

    Added Author to form Article and database.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
  
 	private
 	  def article_params
-	    params.require(:article).permit(:title, :text)
+	    params.require(:article).permit(:title, :text, :author)
 	  end
 
 end
