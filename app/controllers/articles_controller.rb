cb4c520c60d5fcf9e4a12d3c5f4ea0ad0d57b9bf
Noam Lessner <noaml@linux4biz.com>
2015-05-19 14:40:03 +0300

    add delete functionality cleanup code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,29 +8,38 @@ class ArticlesController < ApplicationController
 	    @article = Article.find(params[:id])
 	  end
 
+	  def edit
+	    @article = Article.find(params[:id])
+	  end
+
 	def new
 	end
 
 	def destroy
-	  @article= Article.find(params[:id])
-	  if @article.destroy
-		flash[:notice] = "Successfully deleted message."
-   		 redirect_to articles_path
-
-	  else
-     		flash[:notice] = "NOT Successfully deleted message."
- 	   redirect_to articles_path
-
-	  end
+	    @article= Article.find(params[:id])
+	    @article.destroy
+  	    redirect_to articles_path
 	end
 
 	def create
 	  @article = Article.new(article_params)
-	 
-	  @article.save
-	  redirect_to @article
+ 	  if @article.save
+	  	redirect_to @article
+	  else
+		render 'new'
+	  end
 	end
  
+	def update
+	  @article = Article.find(params[:id])
+	 
+	  if @article.update(article_params)
+	    redirect_to @article
+	  else
+	    render 'edit'
+	  end
+	end
+
 	private
 	  def article_params
 	    params.require(:article).permit(:title, :text, :author)
